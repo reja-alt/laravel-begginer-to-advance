42f83e3e3cc7bf45fd95c126bc2ce2b970e96816
Zillur Rahman <reja6402@gmail.com>
2023-12-29 21:59:13 +0600

    route and view added

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,6 @@
+ 
+<html>
+    <body>
+        <h1>Hello {{ $name }} {{ $age }} {{ $gender }}</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::Redirect('/', 'login');
+// Route::get('login', function() {
+//     return 'Login Page';
+// });
+
+// Route::get('login', function() {
+//     return '<a href="about">About</a>';
+// });
+
+// Route::get('about', function() {
+//     return 'About Page';
+// });
+
+// Route::get('greeting', function() {
+//     $name = "reja";
+//     $age = '29';
+//     return view('greeting', compact('name', 'age'));
+// });
+
+// Route::get('greeting', function() {
+//     $name = "reja";
+//     $age = 29;
+//     $gender = 'male';
+//     return view('greeting', ['name' => $name], ['age' => $age], ['gender' => $gender]);
+// });
+
+Route::get('greeting', function() {
+    $name = "reja";
+    $age = 29;
+    $gender = 'male';
+    return view('greeting')
+                ->with('name', $name)
+                ->with('age', $age)
+                ->with('gender', $gender);
 });
+// Route::view('greeting', 'greeting');
+
+Route::get('test', function() {
+    return view('admin.profile');
+});
+
